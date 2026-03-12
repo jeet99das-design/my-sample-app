4f32a18596213d7305d084d820ce0bc48f5ca65c
Ivan Das <jeet99das@gmail.com>
2026-03-12 18:32:24 +0530

    Simplify greeting message in App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello I'm Ivan! How are you doing!!");
+        System.out.println("Hello I'm Ivan!");
     }
 
     public static String greet(String name) {
