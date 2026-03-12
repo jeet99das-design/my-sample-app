8742f14b98b139d2facb0261bab0448a8c317aaa
Ivan Das <jeet99das@gmail.com>
2026-03-12 18:23:00 +0530

    Update greeting message in App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Iv!");
+        System.out.println("Hello I'm Ivan! How are you doing!!");
     }
 
     public static String greet(String name) {
